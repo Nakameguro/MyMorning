3ea90b6255ebd19e63480d1c931dcfe14393ef22
l08084 <l08084@shibaura-it.ac.jp>
2016-10-01 15:19:20 +0900

    メニュー追加(途中)

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -4,18 +4,78 @@ import { StatusBar } from 'ionic-native';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
+import { Digest } from '../pages/digest/digest';
+import { RealTime } from '../pages/real-time/real-time';
+import { ReadLater } from '../pages/read-later/read-later';
+
+export interface PageObj {
+  title: string;
+  component: any;
+  icon: string;
+  index?: number;
+}
 
 @Component({
-  template: `<ion-nav [root]="rootPage"></ion-nav>`
+  template: `app.template.html`
 })
 export class MyApp {
   rootPage = TabsPage;
 
+  / the root nav is a child of the root app component
+  // @ViewChild(Nav) gets a reference to the app's root nav
+  @ViewChild(Nav) nav: Nav;
+
+  // List of pages that can be navigated to from the left menu
+  // the left menu only works after login
+  // the login page disables the left menu
+  appPages: PageObj[] = [
+    { title: 'Schedule', component: TabsPage, icon: 'calendar' },
+    { title: 'Speakers', component: TabsPage, index: 1, icon: 'contacts' },
+    { title: 'Map', component: TabsPage, index: 2, icon: 'map' },
+    { title: 'About', component: TabsPage, index: 3, icon: 'information-circle' },
+  ];
+  loggedInPages: PageObj[] = [
+    { title: 'Account', component: AccountPage, icon: 'person' },
+    { title: 'Logout', component: TabsPage, icon: 'log-out' }
+  ];
+  loggedOutPages: PageObj[] = [
+    { title: 'Login', component: LoginPage, icon: 'log-in' },
+    { title: 'Signup', component: SignupPage, icon: 'person-add' }
+  ];
+
+
   constructor(platform: Platform) {
+
+
+   public openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    // this.nav.setRoot(page.component);
+  }
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
     });
+  }
+
+  openPage(page: PageObj) {
+    // the nav component was found using @ViewChild(Nav)
+    // reset the nav to remove previous pages and only have this page
+    // we wouldn't want the back button to show in this scenario
+    if (page.index) {
+      this.nav.setRoot(page.component, {tabIndex: page.index});
+
+    } else {
+      this.nav.setRoot(page.component);
+    }
+
+    if (page.title === 'Logout') {
+      // Give the menu time to close before changing to logged out
+      setTimeout(() => {
+        this.userData.logout();
+      }, 1000);
+    }
   }
 }
